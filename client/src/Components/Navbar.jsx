66449ecc53aa20e0da7fedd8c5c70cff4fc0153f
Fazifazz <fasilpambalath7@gmail.com>
2024-03-23 13:19:48 +0530

    image made dynamic

--- client/src/Components/Navbar.jsx
+++ client/src/Components/Navbar.jsx
@@ -1,8 +1,8 @@
-import React from 'react';
-import { useSelector } from 'react-redux';
+import React from "react";
+import { useSelector } from "react-redux";
 
 const Navbar = () => {
-    const { user } = useSelector((state) => state.Auth);
+  const { user } = useSelector((state) => state.Auth);
   return (
     <nav className="flex justify-between items-center bg-gray-800 text-white p-4">
       <div className="flex items-center">
@@ -10,15 +10,13 @@ const Navbar = () => {
       </div>
 
       <div className="flex items-center">
+        
         {/* Profile image */}
         <img
-          src={user?.profile} 
+          src={`/${user?.profile}`}
           alt="Profile"
           className="w-10 h-10 rounded-full mr-2"
         />
-
-        {/* Profile name or username */}
-        <span className="text-sm">{user?.name}</span>
       </div>
     </nav>
   );
